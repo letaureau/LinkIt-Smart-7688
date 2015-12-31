addb14347c10fbc782aab8e53bfff6c200bf73ef
Parth Singh <prs331@nyu.edu>
2015-12-31 19:21:51 +0530

    First Commit

---
+++ Sleep_LCD2.py
@@ -0,0 +1,98 @@
+import sys
+import urllib
+import urllib2
+import json
+import datetime
+from datetime import timedelta
+from time import strptime
+import pyupm_i2clcd as lcd
+import time
+def formatting(entry):
+    t = datetime.datetime.fromtimestamp(float(json.dumps(entry))).strftime('%Y,%m,%d,%H,%M,%S')
+    f_entry = str(t)
+    return t
+# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
+while True:
+    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+    myLcd.setCursor(0,0)
+    user = "user"
+    passwd = "password"
+    link = "https://jawbone.com/nudge/api/users/@me/sleeps?"
+    url = 'https://jawbone.com/user/signin/login'
+    params = urllib.urlencode({
+        'email': user,
+        'pwd': passwd,
+        'service': 'nudge'
+        })
+    tokenresponse = urllib2.urlopen(url, params)
+    data = json.load(tokenresponse)
+    token_num = data["token"]
+    opener = urllib2.build_opener()
+    opener.addheaders.append(('x-nudge-token', token_num))
+    dataresponse = opener.open(link)
+    data = json.load(dataresponse)
+    meta = data['meta']
+    data2 = data['data']
+    items = data['data']['items']
+    size = data['data']['size']
+    item = 0
+    for item in range(0, size):
+        times = json.dumps(items[item]["details"]["awake_time"])
+        times = float(times)
+        times = formatting(times)
+        times2 = json.dumps(items[item]["details"]["asleep_time"])
+        times2 = float(times2)
+        times2 = formatting(times2)
+        times4 = strptime(times2, '%Y,%m,%d,%H,%M,%S')
+        times3 = strptime(times, '%Y,%m,%d,%H,%M,%S')
+        times3 = datetime.datetime(*times3[:6])
+        times4 = datetime.datetime(*times4[:6])
+        myLcd.write("Went to Bed : ")
+        myLcd.setCursor(0,0)
+        #time.sleep(5)
+        myLcd.setCursor(1,0)
+        myLcd.write(times2)
+        time.sleep(5)
+        myLcd.clear()
+        myLcd.setCursor(0,0)
+        myLcd.write("Got UP  : ")
+        #time.sleep(5)
+        myLcd.setCursor(1,0)
+        myLcd.write(times)
+        time.sleep(5)
+        myLcd.clear()
+        #myLcd.setCursor(1,0)
+        #time.sleep(5)
+        #print "asleep time : ", times4
+        #print "awake time  : ", times3
+        uptime = times3 - times4
+        uptime2 = str(uptime)
+        myLcd.setCursor(0,0)
+        myLcd.write("sleep hours :")
+        myLcd.setCursor(1,0)
+        myLcd.write(uptime2)
+        #print "asleep hours :", uptime2  # uptime.total_seconds()," seconds"
+        myLcd.setCursor(1,7)
+        if (uptime.total_seconds() >= 22000):
+            myLcd.write("GOOD SLEEP")
+            time.sleep(5)
+            myLcd.clear()
+        else:
+            myLcd.write("BAD SLEEP")
+            time.sleep(5)
+            myLcd.clear()
+    # RGB Blue
+    #myLcd.setColor(53, 39, 249)
+    #time.sleep(5)
+
+    # RGB Red
+    myLcd.setColor(255, 0, 0)
+    myLcd.setColor(0,255, 0)
+    myLcd.setColor(0,0,255)
+
+#time.sleep(5)
+
+#myLcd.write("asleep time : ", times4)
+#myLcd.setCursor(1,2)
+#myLcd.write("awake time  : ", times3)
+#time.sleep(5)
---
+++ Steps_LCD2.py
@@ -0,0 +1,75 @@
+import sys
+import urllib
+import urllib2
+import json
+import datetime
+from datetime import timedelta
+from time import strptime
+import pyupm_i2clcd as lcd
+import time
+def formatting(entry):
+    t = datetime.datetime.fromtimestamp(float(json.dumps(entry))).strftime('%Y,%m,%d,%H,%M,%S')
+    f_entry = str(t)
+    return t
+# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
+while True:
+    myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+    myLcd.setCursor(0,0)
+    user = "user"
+    passwd = "password"
+    link = "https://jawbone.com/nudge/api/users/@me/moves?"
+    url = 'https://jawbone.com/user/signin/login'
+    params = urllib.urlencode({
+        'email': user,
+        'pwd': passwd,
+        'service': 'nudge'
+        })
+    tokenresponse = urllib2.urlopen(url, params)
+    data = json.load(tokenresponse)
+    token_num = data["token"]
+    opener = urllib2.build_opener()
+    opener.addheaders.append(('x-nudge-token', token_num))
+    dataresponse = opener.open(link)
+    data = json.load(dataresponse)
+    meta = data['meta']
+    data2 = data['data']
+    items = data['data']['items']
+    size = data['data']['size']
+    item = 0
+    for item in range(0, size):
+        dates = items [item]['date']
+        steps = items [item]['title']
+        myLcd.setCursor(0,0)
+        myLcd.write("Dates : ")
+        myLcd.setCursor(1,0)
+        myLcd.write(dates)
+        time.sleep(5)
+        myLcd.clear()
+        myLcd.setCursor(0,0)
+        myLcd.write("Total Steps")
+        myLcd.setCursor(1,0)
+        myLcd.write(steps)
+        myLcd.setCursor(1,7)
+        if (steps >= 10000):
+            myLcd.write("GOOD MOVE")
+            time.sleep(5)
+            myLcd.clear()
+        else:
+            myLcd.write("BAD MOVE")
+            time.sleep(5)
+            myLcd.clear()
+    # RGB Blue
+    #myLcd.setColor(53, 39, 249)
+    #time.sleep(5)
+
+    # RGB Red
+    myLcd.setColor(255, 0, 0)
+    myLcd.setColor(0,255, 0)
+    myLcd.setColor(0,0,255)
+
+#time.sleep(5)
+
+#myLcd.write("asleep time : ", times4)
+#myLcd.setCursor(1,2)
+#myLcd.write("awake time  : ", times3)
+#time.sleep(5)
